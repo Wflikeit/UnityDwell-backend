e30018e347e327f83e2f54b563d378e6bb8cf11b
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-16 16:48:10 +0100

    finished initialization

---
+++ src/main/java/UnityDwell/com/UnityDwell/UnityDwellApplication.java
@@ -0,0 +1,13 @@
+package UnityDwell.com.UnityDwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnityDwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnityDwellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UnityDwell/com/UnityDwell/UnityDwellApplicationTests.java
@@ -0,0 +1,13 @@
+package UnityDwell.com.UnityDwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnityDwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
