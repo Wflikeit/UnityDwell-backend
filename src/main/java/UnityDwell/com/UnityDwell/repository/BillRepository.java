aabf475ade6621b908d68e467e79eb01c4449737
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-19 16:17:03 +0100

    fixed mapping to bill without relations

--- src/main/java/UnityDwell/com/UnityDwell/repository/BillRepository.java
+++ src/main/java/UnityDwell/com/UnityDwell/repository/BillRepository.java
@@ -1,21 +1,24 @@
 package UnityDwell.com.UnityDwell.repository;
 
 import UnityDwell.com.UnityDwell.model.Bill;
-import UnityDwell.com.UnityDwell.model.Resident;
 import UnityDwell.com.UnityDwell.repository.sqlProviders.BillSqlProvider;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Mapper
 @Repository
 public interface BillRepository {
     @SelectProvider(BillSqlProvider.class)
     @Results(id = "billMap", value = {
-            @Result(property = "id",
-                    column = "ID_RACHUNKU")})
+            @Result(property = "id", column = "ID_RACHUNKU"),
+            @Result(property = "amount", column = "KWOTA"),
+            @Result(property = "dateOfPublishing", column = "DATA_WYSTAWIENIA"),})
+//            @Result(property = "amount", column = "KWOTA"),})
     List<Bill> getAllBills();
 //    @SelectProvider(BillSqlProvider.class)
 //    List<Bill> getAllOwnersBills(UUID id);
