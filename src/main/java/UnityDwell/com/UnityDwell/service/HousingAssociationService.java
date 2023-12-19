f88a22bcb3055f205ea1266c608841f5456fa6c1
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-19 21:18:57 +0100

    wapped service method into @transactional

--- src/main/java/UnityDwell/com/UnityDwell/service/HousingAssociationService.java
+++ src/main/java/UnityDwell/com/UnityDwell/service/HousingAssociationService.java
@@ -2,14 +2,12 @@ package UnityDwell.com.UnityDwell.service;
 
 import UnityDwell.com.UnityDwell.dto.HousingAssociationResponse;
 import UnityDwell.com.UnityDwell.dto.mapper.HousingAssociationDTOMapper;
-import UnityDwell.com.UnityDwell.model.Address;
 import UnityDwell.com.UnityDwell.model.HousingAssociation;
-import UnityDwell.com.UnityDwell.repository.AddressRepository;
 import UnityDwell.com.UnityDwell.repository.HousingAssociationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,12 +15,11 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class HousingAssociationService {
     private final HousingAssociationRepository housingAssociationRepository;
-    private final AddressRepository addressRepository;
     private final HousingAssociationDTOMapper housingAssociationDTOMapper;
 
+    @Transactional(readOnly = true)
     public HousingAssociationResponse getHousingAssociation(UUID housingAssociationId) {
 
-//        Optional<Address> address = Optional.of(addressRepository.findAddressById(UUID.fromString("1bce57d4-9a68-4f0d-a39b-ff3fec8fed44")).orElseThrow());
         Optional<HousingAssociation> list = Optional.of(housingAssociationRepository.getHousingAssociation(housingAssociationId).orElseThrow());
         return housingAssociationDTOMapper.mapTo(list.get());
     }
