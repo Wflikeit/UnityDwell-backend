067654a28c04661d4e4035377630d12cc09fb000
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-22 08:03:54 +0100

    fixed HousingAssocation  naming inside method

--- src/main/java/UnityDwell/com/UnityDwell/service/HousingAssociationService.java
+++ src/main/java/UnityDwell/com/UnityDwell/service/HousingAssociationService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,8 +19,9 @@ public class HousingAssociationService {
     @Transactional(readOnly = true)
     public HousingAssociationResponse getHousingAssociationById(UUID housingAssociationId) {
 
-        Optional<HousingAssociation> list = Optional
-                .of(housingAssociationRepository.findByIdHousingAssociation(housingAssociationId).orElseThrow());
-        return housingAssociationDTOMapper.mapTo(list.get());
+
+        HousingAssociation housingAssociation = housingAssociationRepository.findByIdHousingAssociation(housingAssociationId)
+                .orElseThrow();
+        return housingAssociationDTOMapper.mapTo(housingAssociation);
     }
 }
