72df9ecd063d03a4bb938ccacbf9f4ad0b69d8d6
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-23 23:29:41 +0100

    updated request dto for publications

--- src/main/java/UnityDwell/com/UnityDwell/dto/request/CreateOrUpdatePublicationRequest.java
+++ src/main/java/UnityDwell/com/UnityDwell/dto/request/CreateOrUpdatePublicationRequest.java
@@ -1,14 +1,17 @@
 package UnityDwell.com.UnityDwell.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDate;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
 public class CreateOrUpdatePublicationRequest {
+    @NotNull(message = "Title is required.")
+    @Length(min = 2, max = 30, message = "Field must be between {min} and {max} characters long.")
     private String title;
+    @NotNull(message = "Content is required.")
+    @Length(min = 5, max = 100, message = "Field must be between {min} and {max} characters long.")
     private String content;
-    private LocalDate dateOfPublishing;
 }
