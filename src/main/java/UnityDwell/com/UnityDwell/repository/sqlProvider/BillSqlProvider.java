28e48afce0ba641b459238ad53eb6843e48fa34a
avrgprogrammer <mkrajewski0204@gmail.com>
2023-12-28 09:53:56 +0100

    Added a request to collection

--- src/main/java/UnityDwell/com/UnityDwell/repository/sqlProvider/BillSqlProvider.java
+++ src/main/java/UnityDwell/com/UnityDwell/repository/sqlProvider/BillSqlProvider.java
@@ -8,17 +8,17 @@ import java.util.UUID;
 public class BillSqlProvider implements  ProviderMethodResolver{
     public static String getAllBills() {
         return new SQL()
-                .SELECT("r.ID_RACHUNKU", "r.DATA_WYSTAWIENIA", "r.KWOTA", "r.ID_TYTULU_RACHUNKU", "r.ID_SPOLDZIELNI", "r.NR_MIESZKANCA")
+                .SELECT("r.ID_RACHUNKU", "r.DATA_WYSTAWIENIA", "r.KWOTA", "r.ID_TYTULU_RACHUNKU", "r.ID_SPOLDZIELNI", "r.NR_MIESZKANCA", "t.TYTUL")
                 .INNER_JOIN("C##MACIEK.TYTULY_RACHUNKU t ON r.ID_TYTULU_RACHUNKU = t.ID_TYTULU_RACHUNKU")
                 .FROM("C##MACIEK.RACHUNKI r")
                 .toString();
     }
     public String getAllOwnersBills(UUID ownerId) {
         return new SQL()
-                .SELECT("r.ID_RACHUNKU, r.DATA_WYSTAWIENIA, r.KWOTA, r.ID_TYTULU_RACHUNKU, r.ID_SPOLDZIELNI, R.NR_MIESZKANCA")
-                .INNER_JOIN("C##MACIEK.TYTULY_RACHUNKU t ON r.ID_RACHUNKU = t.ID_TYTULU_RACHUNKU")
+                .SELECT("r.ID_RACHUNKU, r.DATA_WYSTAWIENIA, r.KWOTA, r.ID_TYTULU_RACHUNKU, r.ID_SPOLDZIELNI, R.NR_MIESZKANCA", "t.TYTUL")
+                .INNER_JOIN("C##MACIEK.TYTULY_RACHUNKU t ON r.ID_TYTULU_RACHUNKU = t.ID_TYTULU_RACHUNKU")
                 .FROM("C##MACIEK.RACHUNKI r")
-                .WHERE("r.ID_RACHUNKU = #{ownerId}")
+                .WHERE("r.NR_MIESZKANCA = #{ownerId}")
                 .toString();
     }
 
