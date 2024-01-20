4c4f55d4b3765762ee56416220314e2a492cf4d5
avrgprogrammer <mkrajewski0204@gmail.com>
2024-01-20 21:58:45 +0100

    Added POST endpoint for resident plus unit tests

--- src/main/java/UnityDwell/com/UnityDwell/dto/mapper/ResidentDTOMapper.java
+++ src/main/java/UnityDwell/com/UnityDwell/dto/mapper/ResidentDTOMapper.java
@@ -1,13 +1,26 @@
 package UnityDwell.com.UnityDwell.dto.mapper;
 
+import UnityDwell.com.UnityDwell.dto.request.CreateOrUpdateResidentRequest;
 import UnityDwell.com.UnityDwell.dto.response.ResidentResponse;
+import UnityDwell.com.UnityDwell.model.Flat;
 import UnityDwell.com.UnityDwell.model.Resident;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class ResidentDTOMapper {
     public ResidentResponse mapTo(Resident resident){
         return ResidentResponse.builder().id(resident.getId()).name(resident.getName())
                     .surname(resident.getSurname()).flatId(resident.getFlat().getId()).build();
+    }
+
+    public Resident map(CreateOrUpdateResidentRequest request, Flat flat) {
+        return Resident.builder()
+                .id(UUID.randomUUID())
+                .name(request.getName())
+                .surname(request.getSurname())
+                .flat(flat)
+                .build();
     }
 }
