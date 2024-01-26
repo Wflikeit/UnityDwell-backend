734a2b477e4131ee2bd6213504220b224bb1ac8b
avrgprogrammer <mkrajewski0204@gmail.com>
2024-01-26 16:00:27 +0100

    Saving changes

--- src/main/java/UnityDwell/com/UnityDwell/dto/request/CreateOrUpdateBuildingRequest.java
+++ src/main/java/UnityDwell/com/UnityDwell/dto/request/CreateOrUpdateBuildingRequest.java
@@ -36,7 +36,7 @@ public class CreateOrUpdateBuildingRequest {
     @Length(min = 2, max = 50, message = "Field must be between {min} and {max} characters long.")
     private String street;
     @NotNull(message = "Number of building is required.")
-    @Length(min = 2, max = 4, message = "Field must be between {min} and {max} characters long.")
+    @Length(min = 1, max = 5, message = "Field must be between {min} and {max} characters long.")
     private String numberOfBuilding;
     @NotNull(message = "Postal code is required.")
     @Pattern(regexp = "^\\d{2}-\\d{3}$", message = "Invalid postalCode code")
